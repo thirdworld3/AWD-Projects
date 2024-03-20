a4a0d96095c3a082e7a4b7835b08841e1484c7df
third <156496577+thirdworld3@users.noreply.github.com>
2024-03-21 03:41:54 +0800

    Add files via upload

---
+++ Flashlight/Assets/JS/script.js
@@ -0,0 +1,7 @@
+const mouseEvent = document.documentElement;
+
+mouseEvent.addEventListener('mousemove', (e) =>{
+mouseEvent.style.setProperty('--x',e.clientX+'px');
+mouseEvent.style.setProperty('--y',e.clientY+'px');
+
+})
